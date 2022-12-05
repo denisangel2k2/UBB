e64ec59ce8125894dd4f1aaa09c9d8b736292792
Denis Moldovan <57831211+denisangel2k2@users.noreply.github.com>
2022-12-05 17:19:05 +0200

    Added database testing laboratory assignment

--- Year 2/Databases/Scripts/DatabaseTest.sql
+++ Year 2/Databases/Scripts/DatabaseTest.sql
@@ -24,7 +24,8 @@ begin
 	else if @table_name='Coarde'
 	begin
 		--daugam inregistrare ca sa putem face conexiunea
-		insert into ProducatoriCoarde (nume_prod) values ('Producator')
+		if not exists(select * from ProducatoriCoarde where nume_prod='Producator')
+			insert into ProducatoriCoarde (nume_prod) values ('Producator')
 			
 		declare @id_prodd int
 		select top(1) @id_prodd=id_prod from ProducatoriCoarde
@@ -131,41 +132,99 @@ end
 
 
 select * from tests
+
+
+
+
+
+
+
+create or alter procedure ExecuteView
+@view_name varchar(30)
+as
+begin
+
+	declare @sqlstatement nvarchar(150)
+	if exists(select name from sys.views where name=@view_name)
+		begin
+			set @sqlstatement = N'select * from '+@view_name
+			exec sp_executesql @sqlstatement
+		end;
+end
+
+
+exec TestDatabase
+
+select * from TestRuns
+select * from TestRunTables
+select * from TestRunViews
+
 create or alter procedure TestDatabase
 as
 begin
 
+
+	
 	set nocount on
-	declare @maximumTestId int
+
+	
+	delete from TestRuns
+	delete from TestRunViews
+	delete from TestRunTables
+	DBCC CHECKIDENT (TestRuns, RESEED, 0);
+	-- delete all from the tables
+
+		declare cursorPreDelete cursor forward_only
+		for
+		select T.name from TestTables TT
+			inner join Tables T on T.TableID=TT.TableID
+			where TT.TestID = 3
+			order by Position
+		declare @tableTempDeleteName varchar(25)
+		open cursorPreDelete
+			fetch next from cursorPreDelete into @tableTempDeleteName
+			while @@FETCH_STATUS=0
+			begin
+				exec DeleteFromTable @tableTempDeleteName
+				fetch next from cursorPreDelete into @tableTempDeleteName
+			end;
+
+		close cursorPreDelete
+		deallocate cursorPreDelete
+
 	declare @currentTestId int
-
-	select @maximumTestId=max(TestID) from Tests 
-	select @currentTestId=min(TestID) from Tests 
-
-	print @maximumTestId
-
 	declare @tableId int
 	declare @tableName varchar(25)
 	declare @noOfRows int
 	
 	declare @startGlobal datetime
-	set @startGlobal = getdate()
-
-	insert into TestRuns(StartAt) values (@startGlobal)
-
-	declare @maxTestRunId int
-	select @maxTestRunId=max(TestRunID) from TestRuns;
 	
+
+	
+
+	declare @startTime datetime, @endTime datetime
 	declare @endGlobal datetime
+	declare @description nvarchar(50)
+	declare @TestRunId int
+	----- here starts
+	declare cursorTest cursor forward_only for
+	select TestID, Name from Tests
 
+	open cursorTest
 
-	while @currentTestId<=@maximumTestId
+	fetch next from cursorTest into @currentTestId, @description
+	while @@FETCH_STATUS=0
 	begin
+		insert into TestRuns(Description,StartAt) values (@description,getdate())
+
+		
+		set @TestRunId = @@IDENTITY;
+
 		declare cursorForDelete cursor forward_only for
 		select T.name, T.TableID from TestTables TT
 		inner join Tables T on T.TableID=TT.TableID
 		where TT.TestID = @currentTestId
-		order by Position desc
+		order by Position
 
 		declare @testStart datetime
 		set @testStart = getdate()
@@ -189,14 +248,20 @@ begin
 		select T.name, TT.NoOfRows, T.TableID from TestTables TT
 		inner join Tables T on T.TableID=TT.TableID
 		where TT.TestID = @currentTestId
-		order by Position
+		order by Position desc
 
 		open cursorForInsert
 			fetch next from cursorForInsert into @tableName, @noOfRows, @tableId
 			while @@FETCH_STATUS=0
 			begin
 				--aici masor timpul de executie pentru insert al fiecarui tabel si il stochez in TestRunTables
+				
+				
+				
+				set @startTime=GETDATE()
 				exec InsertINTO @tableName, @noOfRows
+				set @endTime=GETDATE()
+				insert into TestRunTables(TestRunID,TableID,StartAt,EndAt) values (@TestRunId,@tableId,@startTime,@endTime);
 
 				fetch next from cursorForInsert into @tableName, @noOfRows, @tableId
 			end
@@ -204,22 +269,38 @@ begin
 		close cursorForInsert
 		deallocate cursorForInsert
 
-		declare @testEnd datetime
-		set @testEnd = getdate();
+
+		declare @viewName nvarchar(100);
+		declare @viewIDFrom int;
+
+		declare cursorViews cursor forward_only
+		for
+		select V.name, V.ViewID from TestViews TV
+		inner join Views V on V.ViewID=TV.ViewID
+		where TV.TestID = @currentTestId
 		
-	
+		open cursorViews
+			fetch next from cursorViews into @viewName,@viewIDFrom
+			while @@FETCH_STATUS = 0
+			begin
+				set @startTime=GETDATE();
+				exec ExecuteView @viewName
+				set @endTime=GETDATE();
 
+				insert into TestRunViews(TestRunID,ViewID,StartAt,EndAt) values (@TestRunId,@viewIDFrom,@startTime,@endTime)
+				
+				fetch next from cursorViews into @viewName,@viewIDFrom
+			end;
+		close cursorViews
+		deallocate cursorViews
 
-		set @currentTestId=@currentTestId+1
+		set @endGlobal = GETDATE()
+		UPDATE TestRuns SET EndAt=@endGlobal where TestRunID=@TestRunId;
 
-		--aici mai fac si pentru
+		fetch next from cursorTest into @currentTestId, @description
+
 	end;
+	close cursorTest
+	deallocate cursorTest
 	
-
-
-end
-
-
-
-exec TestDatabase
-exec DeleteFromTable 'ProducatoriCoarde'
+end;
